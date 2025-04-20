3a1ba7fa824b302073a1d9df587a32811c1469c1
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-20 16:14:09 +0400

    drop usage of EP_NAME

--- api/src/main/java/consulo/msbuild/bundle/MSBuildBundleType.java
+++ api/src/main/java/consulo/msbuild/bundle/MSBuildBundleType.java
@@ -17,6 +17,7 @@
 package consulo.msbuild.bundle;
 
 import consulo.annotation.component.ExtensionImpl;
+import consulo.application.Application;
 import consulo.application.util.SystemInfo;
 import consulo.msbuild.MSBuildVersion;
 import consulo.msbuild.icon.MSBuildIconGroup;
@@ -27,9 +28,9 @@ import consulo.process.util.CapturingProcessUtil;
 import consulo.process.util.ProcessOutput;
 import consulo.ui.image.Image;
 import consulo.util.collection.ContainerUtil;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -42,167 +43,138 @@ import java.util.stream.Collectors;
  * @since 08.06.2015
  */
 @ExtensionImpl
-public class MSBuildBundleType extends BaseMSBuildBundleType
-{
-	public static class MSBuildInfo
-	{
-		private File myPath;
-		private String myBitness;
-		private MSBuildVersion myVersion;
-		@Nullable
-		private String myEdition;
+public class MSBuildBundleType extends BaseMSBuildBundleType {
+    public static class MSBuildInfo {
+        private File myPath;
+        private String myBitness;
+        private MSBuildVersion myVersion;
+        @Nullable
+        private String myEdition;
 
-		public MSBuildInfo(File path, String bitness, MSBuildVersion version, @Nullable String edition)
-		{
-			myPath = path;
-			myBitness = bitness;
-			myVersion = version;
-			myEdition = edition;
-		}
+        public MSBuildInfo(File path, String bitness, MSBuildVersion version, @Nullable String edition) {
+            myPath = path;
+            myBitness = bitness;
+            myVersion = version;
+            myEdition = edition;
+        }
 
-		public File getPath()
-		{
-			return myPath;
-		}
+        public File getPath() {
+            return myPath;
+        }
 
-		public String buildName()
-		{
-			StringBuilder builder = new StringBuilder();
-			builder.append("MSBuild ");
-			builder.append(myVersion.getInternalVersion());
-			if(myEdition != null)
-			{
-				builder.append(" [VS ");
-				builder.append(myVersion.getYearVersion());
-				builder.append(" ");
-				builder.append(myEdition);
-				builder.append(" ");
-				builder.append(myBitness);
-				builder.append("]");
-			}
-			return builder.toString();
-		}
-	}
+        public String buildName() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("MSBuild ");
+            builder.append(myVersion.getInternalVersion());
+            if (myEdition != null) {
+                builder.append(" [VS ");
+                builder.append(myVersion.getYearVersion());
+                builder.append(" ");
+                builder.append(myEdition);
+                builder.append(" ");
+                builder.append(myBitness);
+                builder.append("]");
+            }
+            return builder.toString();
+        }
+    }
 
-	@Nonnull
-	public static String getExecutable(String home)
-	{
-		return home + "/bin/MSBuild.exe";
-	}
+    @Nonnull
+    public static String getExecutable(String home) {
+        return home + "/bin/MSBuild.exe";
+    }
 
-	@Nonnull
-	public static MSBuildBundleType getInstance()
-	{
-		return EP_NAME.findExtensionOrFail(MSBuildBundleType.class);
-	}
+    @Nonnull
+    public static MSBuildBundleType getInstance() {
+        return Application.get().getExtensionPoint(MSBuildBundleType.class).findExtensionOrFail(MSBuildBundleType.class);
+    }
 
-	public MSBuildBundleType()
-	{
-		super("MSBUILD_BUNDLE");
-	}
+    public MSBuildBundleType() {
+        super("MSBUILD_BUNDLE");
+    }
 
-	@Nonnull
-	@Override
-	public Collection<String> suggestHomePaths()
-	{
-		return findMSBuilds().stream().map(msBuildInfo -> msBuildInfo.getPath().getPath()).collect(Collectors.toList());
-	}
+    @Nonnull
+    @Override
+    public Collection<String> suggestHomePaths() {
+        return findMSBuilds().stream().map(msBuildInfo -> msBuildInfo.getPath().getPath()).collect(Collectors.toList());
+    }
 
-	@Nonnull
-	public List<MSBuildInfo> findMSBuilds()
-	{
-		if(!SystemInfo.isWindows)
-		{
-			return Collections.emptyList();
-		}
+    @Nonnull
+    public List<MSBuildInfo> findMSBuilds() {
+        if (!SystemInfo.isWindows) {
+            return Collections.emptyList();
+        }
 
-		List<MSBuildInfo> list = new ArrayList<>();
+        List<MSBuildInfo> list = new ArrayList<>();
 
-		collectVisualStudioCompilerPaths(list, "ProgramFiles", SystemInfo.is64Bit ? "x64" : "x86");
-		collectVisualStudioCompilerPaths(list, "ProgramFiles(x86)", "x86");
+        collectVisualStudioCompilerPaths(list, "ProgramFiles", SystemInfo.is64Bit ? "x64" : "x86");
+        collectVisualStudioCompilerPaths(list, "ProgramFiles(x86)", "x86");
 
-		return list;
-	}
+        return list;
+    }
 
-	private void collectVisualStudioCompilerPaths(List<MSBuildInfo> list, String env, String bitness)
-	{
-		String programFiles = Platform.current().os().getEnvironmentVariable(env);
-		if(programFiles != null)
-		{
-			File msbuildDir = new File(programFiles, "MSBuild");
+    private void collectVisualStudioCompilerPaths(List<MSBuildInfo> list, String env, String bitness) {
+        String programFiles = Platform.current().os().getEnvironmentVariable(env);
+        if (programFiles != null) {
+            File msbuildDir = new File(programFiles, "MSBuild");
 
-			if(msbuildDir.exists())
-			{
-				for(MSBuildVersion version : MSBuildVersion.values())
-				{
-					File compilerPath = new File(msbuildDir, version.getInternalVersion());
-					if(compilerPath.exists())
-					{
-						list.add(new MSBuildInfo(compilerPath, bitness, version, null));
-					}
-				}
-			}
+            if (msbuildDir.exists()) {
+                for (MSBuildVersion version : MSBuildVersion.values()) {
+                    File compilerPath = new File(msbuildDir, version.getInternalVersion());
+                    if (compilerPath.exists()) {
+                        list.add(new MSBuildInfo(compilerPath, bitness, version, null));
+                    }
+                }
+            }
 
-			File vsDirectory = new File(programFiles, "Microsoft Visual Studio");
-			if(vsDirectory.exists())
-			{
-				for(MSBuildVersion version : MSBuildVersion.values())
-				{
-					for(String visualStudioEdition : MSBuildVersion.ourVisualStudioEditions)
-					{
-						File vsTargetDirectory = new File(vsDirectory, version.getYearVersion() + "/" + visualStudioEdition);
-						if(vsTargetDirectory.exists())
-						{
-							File msBuildDirectory = new File(vsTargetDirectory, "MSBuild/" + version.getInternalVersion());
-							if(msBuildDirectory.exists())
-							{
-								list.add(new MSBuildInfo(msBuildDirectory, bitness, version, visualStudioEdition));
-							}
+            File vsDirectory = new File(programFiles, "Microsoft Visual Studio");
+            if (vsDirectory.exists()) {
+                for (MSBuildVersion version : MSBuildVersion.values()) {
+                    for (String visualStudioEdition : MSBuildVersion.ourVisualStudioEditions) {
+                        File vsTargetDirectory = new File(vsDirectory, version.getYearVersion() + "/" + visualStudioEdition);
+                        if (vsTargetDirectory.exists()) {
+                            File msBuildDirectory = new File(vsTargetDirectory, "MSBuild/" + version.getInternalVersion());
+                            if (msBuildDirectory.exists()) {
+                                list.add(new MSBuildInfo(msBuildDirectory, bitness, version, visualStudioEdition));
+                            }
 
-							msBuildDirectory = new File(vsTargetDirectory, "MSBuild/Current");
-							if(msBuildDirectory.exists())
-							{
-								list.add(new MSBuildInfo(msBuildDirectory, bitness, version, visualStudioEdition));
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+                            msBuildDirectory = new File(vsTargetDirectory, "MSBuild/Current");
+                            if (msBuildDirectory.exists()) {
+                                list.add(new MSBuildInfo(msBuildDirectory, bitness, version, visualStudioEdition));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-	@Override
-	public boolean isValidSdkHome(String path)
-	{
-		return new File(getExecutable(path)).exists();
-	}
+    @Override
+    public boolean isValidSdkHome(String path) {
+        return new File(getExecutable(path)).exists();
+    }
 
-	@Nullable
-	@Override
-	public String getVersionString(String sdkHome)
-	{
-		try
-		{
-			ProcessOutput processOutput = CapturingProcessUtil.execAndGetOutput(new GeneralCommandLine(getExecutable(sdkHome), "/version").withWorkDirectory(sdkHome));
-			return ContainerUtil.getLastItem(processOutput.getStdoutLines());
-		}
-		catch(ExecutionException e)
-		{
-			return null;
-		}
-	}
+    @Nullable
+    @Override
+    public String getVersionString(String sdkHome) {
+        try {
+            ProcessOutput processOutput = CapturingProcessUtil.execAndGetOutput(new GeneralCommandLine(getExecutable(sdkHome), "/version").withWorkDirectory(sdkHome));
+            return ContainerUtil.getLastItem(processOutput.getStdoutLines());
+        }
+        catch (ExecutionException e) {
+            return null;
+        }
+    }
 
-	@Nonnull
-	@Override
-	public String getPresentableName()
-	{
-		return "MSBuild";
-	}
+    @Nonnull
+    @Override
+    public String getPresentableName() {
+        return "MSBuild";
+    }
 
-	@Nullable
-	@Override
-	public Image getIcon()
-	{
-		return MSBuildIconGroup.msbuild();
-	}
+    @Nullable
+    @Override
+    public Image getIcon() {
+        return MSBuildIconGroup.msbuild();
+    }
 }
--- impl/dotnet-mono/src/main/java/consulo/msbuild/dotnet/mono/bundle/MonoMSBuildBundleType.java
+++ impl/dotnet-mono/src/main/java/consulo/msbuild/dotnet/mono/bundle/MonoMSBuildBundleType.java
@@ -1,14 +1,15 @@
 package consulo.msbuild.dotnet.mono.bundle;
 
 import consulo.annotation.component.ExtensionImpl;
+import consulo.application.Application;
 import consulo.content.bundle.SdkType;
 import consulo.dotnet.module.extension.AssemblyInfoCacheService;
 import consulo.internal.dotnet.asm.mbel.AssemblyInfo;
 import consulo.mono.dotnet.sdk.MonoSdkType;
 import consulo.ui.image.Image;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
@@ -20,111 +21,92 @@ import java.util.Set;
  * @since 21/01/2021
  */
 @ExtensionImpl
-public class MonoMSBuildBundleType extends SdkType
-{
-	@Nonnull
-	public static MonoMSBuildBundleType getInstance()
-	{
-		return EP_NAME.findExtensionOrFail(MonoMSBuildBundleType.class);
-	}
+public class MonoMSBuildBundleType extends SdkType {
+    @Nonnull
+    public static MonoMSBuildBundleType getInstance() {
+        return Application.get().getExtensionPoint(SdkType.class).findExtensionOrFail(MonoMSBuildBundleType.class);
+    }
 
-	public static File getMSBuildDll(String path)
-	{
-		return new File(path, "bin/MSBuild.dll");
-	}
+    public static File getMSBuildDll(String path) {
+        return new File(path, "bin/MSBuild.dll");
+    }
 
-	public MonoMSBuildBundleType()
-	{
-		super("MONO_MSBUILD_BUNDLE");
-	}
+    public MonoMSBuildBundleType() {
+        super("MONO_MSBUILD_BUNDLE");
+    }
 
-	@Nonnull
-	@Override
-	public Collection<String> suggestHomePaths()
-	{
-		Collection<String> paths = MonoSdkType.getInstance().suggestHomePaths();
+    @Nonnull
+    @Override
+    public Collection<String> suggestHomePaths() {
+        Collection<String> paths = MonoSdkType.getInstance().suggestHomePaths();
 
-		Set<String> msBuildPath = new LinkedHashSet<>();
-		for(String monoPath : paths)
-		{
-			File mono = new File(monoPath, "../msbuild/");
+        Set<String> msBuildPath = new LinkedHashSet<>();
+        for (String monoPath : paths) {
+            File mono = new File(monoPath, "../msbuild/");
 
-			try
-			{
-				String canonicalPath = mono.getCanonicalPath();
+            try {
+                String canonicalPath = mono.getCanonicalPath();
 
-				msBuildPath.add(canonicalPath);
-			}
-			catch(IOException ignored)
-			{
-			}
-		}
+                msBuildPath.add(canonicalPath);
+            }
+            catch (IOException ignored) {
+            }
+        }
 
-		Set<String> msBuildPaths = new LinkedHashSet<>();
-		for(String p : msBuildPath)
-		{
-			// dir msbuild inside mono, it's holder
-			File msRoot = new File(p);
-			File[] files = msRoot.listFiles();
-			if(files != null)
-			{
-				for (File file : files)
-				{
-					msBuildPaths.add(file.getAbsolutePath());
-				}
-			}
-		}
-		return msBuildPaths;
-	}
+        Set<String> msBuildPaths = new LinkedHashSet<>();
+        for (String p : msBuildPath) {
+            // dir msbuild inside mono, it's holder
+            File msRoot = new File(p);
+            File[] files = msRoot.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    msBuildPaths.add(file.getAbsolutePath());
+                }
+            }
+        }
+        return msBuildPaths;
+    }
 
-	@Override
-	public boolean canCreatePredefinedSdks()
-	{
-		return true;
-	}
+    @Override
+    public boolean canCreatePredefinedSdks() {
+        return true;
+    }
 
-	@Override
-	public boolean isValidSdkHome(String path)
-	{
-		return getMSBuildDll(path).exists();
-	}
+    @Override
+    public boolean isValidSdkHome(String path) {
+        return getMSBuildDll(path).exists();
+    }
 
-	@Nullable
-	@Override
-	public String getVersionString(String sdkHome)
-	{
-		AssemblyInfo assemblyInfo = AssemblyInfoCacheService.getInstance().getAssemblyInfo(getMSBuildDll(sdkHome));
-		if(assemblyInfo == null)
-		{
-			return null;
-		}
-		return assemblyInfo.getMajorVersion() + "." + assemblyInfo.getMinorVersion();
-	}
+    @Nullable
+    @Override
+    public String getVersionString(String sdkHome) {
+        AssemblyInfo assemblyInfo = AssemblyInfoCacheService.getInstance().getAssemblyInfo(getMSBuildDll(sdkHome));
+        if (assemblyInfo == null) {
+            return null;
+        }
+        return assemblyInfo.getMajorVersion() + "." + assemblyInfo.getMinorVersion();
+    }
 
-	@Override
-	public String suggestSdkName(String currentSdkName, String sdkHome)
-	{
-		return getPresentableName() + " " + getVersionString(sdkHome);
-	}
+    @Override
+    public String suggestSdkName(String currentSdkName, String sdkHome) {
+        return getPresentableName() + " " + getVersionString(sdkHome);
+    }
 
-	@Nonnull
-	@Override
-	public String getPresentableName()
-	{
-		return "Mono MSBuild";
-	}
+    @Nonnull
+    @Override
+    public String getPresentableName() {
+        return "Mono MSBuild";
+    }
 
-	@Nullable
-	@Override
-	public Image getGroupIcon()
-	{
-		return MonoSdkType.getInstance().getGroupIcon();
-	}
+    @Nullable
+    @Override
+    public Image getGroupIcon() {
+        return MonoSdkType.getInstance().getGroupIcon();
+    }
 
-	@Nullable
-	@Override
-	public Image getIcon()
-	{
-		return MonoSdkType.getInstance().getIcon();
-	}
+    @Nullable
+    @Override
+    public Image getIcon() {
+        return MonoSdkType.getInstance().getIcon();
+    }
 }
