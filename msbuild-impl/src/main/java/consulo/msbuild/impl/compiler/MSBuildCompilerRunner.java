dc3b59af708eefb4e036f30d36ac5e6ee48e5520
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-05-15 17:32:17 +0400

    report errors from msbuild to Consulo
    fixed #32

--- msbuild-impl/src/main/java/consulo/msbuild/impl/compiler/MSBuildCompilerRunner.java
+++ msbuild-impl/src/main/java/consulo/msbuild/impl/compiler/MSBuildCompilerRunner.java
@@ -3,13 +3,13 @@ package consulo.msbuild.impl.compiler;
 import consulo.annotation.component.ExtensionImpl;
 import consulo.application.progress.ProgressIndicator;
 import consulo.application.progress.ProgressIndicatorProvider;
-import consulo.compiler.CompileContextEx;
-import consulo.compiler.CompileDriver;
-import consulo.compiler.CompilerRunner;
-import consulo.compiler.ExitException;
+import consulo.compiler.*;
 import consulo.localize.LocalizeValue;
 import consulo.module.extension.ModuleExtensionHelper;
+import consulo.msbuild.daemon.impl.MSBuildDaemonContext;
 import consulo.msbuild.daemon.impl.MSBuildDaemonService;
+import consulo.msbuild.daemon.impl.message.model.MSBuildTargetResult;
+import consulo.msbuild.daemon.impl.message.model.RunProjectResponse;
 import consulo.msbuild.daemon.impl.step.DaemonStepQueue;
 import consulo.msbuild.daemon.impl.step.InitializeProjectStep;
 import consulo.msbuild.daemon.impl.step.RunTargetProjectStep;
@@ -18,8 +18,11 @@ import consulo.msbuild.module.extension.MSBuildSolutionModuleExtension;
 import consulo.msbuild.solution.model.WProject;
 import consulo.project.Project;
 import consulo.ui.image.Image;
+import consulo.virtualFileSystem.util.VirtualFileUtil;
 import jakarta.annotation.Nonnull;
 import jakarta.inject.Inject;
+
+import java.io.File;
 
 /**
  * @author VISTALL
@@ -52,8 +55,13 @@ public class MSBuildCompilerRunner implements CompilerRunner {
     }
 
     @Override
-    public boolean build(CompileDriver compileDriver, CompileContextEx context, boolean isRebuild, boolean forceCompile, boolean onlyCheckStatus) throws ExitException {
-        MSBuildSolutionModuleExtension<?> solutionExtension = MSBuildSolutionModuleExtension.getSolutionModuleExtension(context.getProject());
+    public boolean build(CompileDriver compileDriver,
+                         CompileContextEx compileContext,
+                         boolean isRebuild,
+                         boolean forceCompile,
+                         boolean onlyCheckStatus) throws ExitException {
+        MSBuildSolutionModuleExtension<?> solutionExtension =
+            MSBuildSolutionModuleExtension.getSolutionModuleExtension(compileContext.getProject());
 
         if (solutionExtension == null) {
             return false;
@@ -66,10 +74,37 @@ public class MSBuildCompilerRunner implements CompilerRunner {
         }
 
         for (WProject project : solutionExtension.getValidProjects()) {
-            steps.join(new RunTargetProjectStep(project, "Build", false));
+            steps.join(new RunTargetProjectStep(project, "Build", false) {
+                @Override
+                public void handleResponse(@Nonnull MSBuildDaemonContext context, @Nonnull RunProjectResponse runProjectResponse) {
+                    super.handleResponse(context, runProjectResponse);
+
+                    if (runProjectResponse.Result == null || runProjectResponse.Result.errors == null) {
+                        return;
+                    }
+
+                    for (MSBuildTargetResult error : runProjectResponse.Result.errors) {
+                        CompilerMessageCategory category = CompilerMessageCategory.ERROR;
+                        if (error.IsWarning) {
+                            category = CompilerMessageCategory.WARNING;
+                        }
+
+                        String filePath = error.File;
+
+                        String fileUrl = null;
+                        if (filePath != null) {
+                            File file = new File(filePath);
+                            if (file.exists()) {
+                                fileUrl = VirtualFileUtil.pathToUrl(filePath);
+                            }
+                        }
+                        compileContext.addMessage(category, error.Message, fileUrl, error.LineNumber, error.ColumnNumber);
+                    }
+                }
+            });
         }
 
-        MSBuildDaemonService daemonService = MSBuildDaemonService.getInstance(context.getProject());
+        MSBuildDaemonService daemonService = MSBuildDaemonService.getInstance(compileContext.getProject());
 
         ProgressIndicator indicator = ProgressIndicatorProvider.getGlobalProgressIndicator();
 
