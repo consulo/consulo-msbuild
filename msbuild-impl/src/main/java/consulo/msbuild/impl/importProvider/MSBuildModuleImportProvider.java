f3b9e6362caec3ae5ea4be9b4a00e7dbde1a7975
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-08-15 19:12:41 +0400

    migrate to localize value

--- msbuild-impl/src/main/java/consulo/msbuild/impl/importProvider/MSBuildModuleImportProvider.java
+++ msbuild-impl/src/main/java/consulo/msbuild/impl/importProvider/MSBuildModuleImportProvider.java
@@ -5,8 +5,8 @@ import consulo.annotation.component.ExtensionImpl;
 import consulo.application.Application;
 import consulo.application.WriteAction;
 import consulo.application.dumb.DumbAwareRunnable;
-import consulo.application.util.HtmlBuilder;
 import consulo.ide.moduleImport.ModuleImportProvider;
+import consulo.localize.LocalizeValue;
 import consulo.module.ModifiableModuleModel;
 import consulo.module.Module;
 import consulo.module.content.layer.ModifiableRootModel;
@@ -23,11 +23,10 @@ import consulo.util.io.FileUtil;
 import consulo.virtualFileSystem.LocalFileSystem;
 import consulo.virtualFileSystem.VirtualFile;
 import consulo.virtualFileSystem.util.VirtualFileUtil;
-import jakarta.inject.Inject;
-import org.intellij.lang.annotations.Language;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+import jakarta.inject.Inject;
+
 import java.io.File;
 import java.util.Set;
 import java.util.function.Consumer;
@@ -37,159 +36,121 @@ import java.util.function.Consumer;
  * @since 23/01/2021
  */
 @ExtensionImpl
-public class MSBuildModuleImportProvider implements ModuleImportProvider<MSBuildModuleImportContext>
-{
-	private final Application myApplication;
+public class MSBuildModuleImportProvider implements ModuleImportProvider<MSBuildModuleImportContext> {
+    private final Application myApplication;
 
-	@Inject
-	public MSBuildModuleImportProvider(Application application)
-	{
-		myApplication = application;
-	}
+    @Inject
+    public MSBuildModuleImportProvider(Application application) {
+        myApplication = application;
+    }
 
-	@Nonnull
-	@Override
-	public MSBuildModuleImportContext createContext(@Nullable Project project)
-	{
-		return new MSBuildModuleImportContext(project);
-	}
+    @Nonnull
+    @Override
+    public MSBuildModuleImportContext createContext(@Nullable Project project) {
+        return new MSBuildModuleImportContext(project);
+    }
 
-	@Nonnull
-	@Override
-	@Language("HTML")
-	public String getFileSample()
-	{
-		HtmlBuilder builder = new HtmlBuilder();
-		builder.wrapWith("b").addText("MSBuild");
-		builder.append("project files (");
-		for(String ext : MSBuildProjectFile.listAll(myApplication))
-		{
-			builder.append("*." + ext);
-		}
-		builder.append(")");
-		return builder.toString();
-	}
+    @Nonnull
+    @Override
+    public LocalizeValue getName() {
+        return LocalizeValue.localizeTODO("MSBuild");
+    }
 
-	@Nonnull
-	@Override
-	public String getName()
-	{
-		return "MSBuild";
-	}
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return MSBuildIconGroup.msbuild();
+    }
 
-	@Nonnull
-	@Override
-	public Image getIcon()
-	{
-		return MSBuildIconGroup.msbuild();
-	}
+    @Override
+    public boolean canImport(@Nonnull File fileOrDirectory) {
+        // if target directory contains Solution - do not allow import as MSBuild
+        if (fileOrDirectory.isDirectory()) {
+            if (SolutionModuleImportProvider.findSingleSolutionFile(fileOrDirectory) != null) {
+                return false;
+            }
+        }
 
-	@Override
-	public boolean canImport(@Nonnull File fileOrDirectory)
-	{
-		// if target directory contains Solution - do not allow import as MSBuild
-		if(fileOrDirectory.isDirectory())
-		{
-			if(SolutionModuleImportProvider.findSingleSolutionFile(fileOrDirectory) != null)
-			{
-				return false;
-			}
-		}
+        if (fileOrDirectory.isDirectory()) {
+            return findSingleSolutionFile(fileOrDirectory) != null;
+        }
+        else {
+            String ext = FileUtil.getExtension(fileOrDirectory.getName());
+            return MSBuildProjectFile.listAll(myApplication).contains(ext);
+        }
+    }
 
-		if(fileOrDirectory.isDirectory())
-		{
-			return findSingleSolutionFile(fileOrDirectory) != null;
-		}
-		else
-		{
-			String ext = FileUtil.getExtension(fileOrDirectory.getName());
-			return MSBuildProjectFile.listAll(myApplication).contains(ext);
-		}
-	}
+    @Override
+    public String getPathToBeImported(@Nonnull VirtualFile file) {
+        if (file.isDirectory()) {
+            File solutionFile = findSingleSolutionFile(VirtualFileUtil.virtualToIoFile(file));
+            if (solutionFile != null) {
+                return solutionFile.getPath();
+            }
+        }
+        return file.getPath();
+    }
 
-	@Override
-	public String getPathToBeImported(@Nonnull VirtualFile file)
-	{
-		if(file.isDirectory())
-		{
-			File solutionFile = findSingleSolutionFile(VirtualFileUtil.virtualToIoFile(file));
-			if(solutionFile != null)
-			{
-				return solutionFile.getPath();
-			}
-		}
-		return file.getPath();
-	}
+    private File findSingleSolutionFile(File directory) {
+        Set<String> exts = MSBuildProjectFile.listAll(myApplication);
 
-	private File findSingleSolutionFile(File directory)
-	{
-		Set<String> exts = MSBuildProjectFile.listAll(myApplication);
+        File firstSolution = null;
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        String ext = FileUtil.getExtension(file.getName());
+                        if (exts.contains(ext)) {
+                            // already found - return null
+                            if (firstSolution != null) {
+                                return null;
+                            }
 
-		File firstSolution = null;
-		if(directory.isDirectory())
-		{
-			File[] files = directory.listFiles();
-			if(files != null)
-			{
-				for(File file : files)
-				{
-					if(file.isFile())
-					{
-						String ext = FileUtil.getExtension(file.getName());
-						if(exts.contains(ext))
-						{
-							// already found - return null
-							if(firstSolution != null)
-							{
-								return null;
-							}
+                            firstSolution = file;
+                        }
+                    }
+                }
+            }
+        }
+        return firstSolution;
+    }
 
-							firstSolution = file;
-						}
-					}
-				}
-			}
-		}
-		return firstSolution;
-	}
+    @Override
+    public void buildSteps(@Nonnull Consumer<WizardStep<MSBuildModuleImportContext>> consumer, @Nonnull MSBuildModuleImportContext context) {
+        consumer.accept(new MSBuildProjectOrModuleNameStep<>(context));
+    }
 
-	@Override
-	public void buildSteps(@Nonnull Consumer<WizardStep<MSBuildModuleImportContext>> consumer, @Nonnull MSBuildModuleImportContext context)
-	{
-		consumer.accept(new MSBuildProjectOrModuleNameStep<>(context));
-	}
+    @RequiredReadAction
+    @Override
+    public void process(@Nonnull MSBuildModuleImportContext context,
+                        @Nonnull Project project,
+                        @Nonnull ModifiableModuleModel modifiableModuleModel,
+                        @Nonnull Consumer<Module> consumer) {
+        String fileToImport = context.getFileToImport();
 
-	@RequiredReadAction
-	@Override
-	public void process(@Nonnull MSBuildModuleImportContext context,
-						@Nonnull Project project,
-						@Nonnull ModifiableModuleModel modifiableModuleModel,
-						@Nonnull Consumer<Module> consumer)
-	{
-		String fileToImport = context.getFileToImport();
+        VirtualFile projectFile = LocalFileSystem.getInstance().findFileByPath(fileToImport);
+        assert projectFile != null;
 
-		VirtualFile projectFile = LocalFileSystem.getInstance().findFileByPath(fileToImport);
-		assert projectFile != null;
+        VirtualFile parent = projectFile.getParent();
 
-		VirtualFile parent = projectFile.getParent();
+        final ModifiableRootModel mainModuleModel = SolutionModuleImportProvider.createModuleWithSingleContent(parent.getName(), parent, modifiableModuleModel);
 
-		final ModifiableRootModel mainModuleModel = SolutionModuleImportProvider.createModuleWithSingleContent(parent.getName(), parent, modifiableModuleModel);
+        MSBuildSolutionMutableModuleExtension<?> solExtension = mainModuleModel.getExtensionWithoutCheck(context.getProvider().getSolutionModuleExtensionId());
+        assert solExtension != null;
+        solExtension.setEnabled(true);
+        solExtension.setProjectFileUrl(projectFile.getUrl());
+        solExtension.setSdkName(context.getMSBuildBundleName());
+        solExtension.setProcessProviderId(context.getProvider().getId());
 
-		MSBuildSolutionMutableModuleExtension<?> solExtension = mainModuleModel.getExtensionWithoutCheck(context.getProvider().getSolutionModuleExtensionId());
-		assert solExtension != null;
-		solExtension.setEnabled(true);
-		solExtension.setProjectFileUrl(projectFile.getUrl());
-		solExtension.setSdkName(context.getMSBuildBundleName());
-		solExtension.setProcessProviderId(context.getProvider().getId());
+        WriteAction.run(mainModuleModel::commit);
 
-		WriteAction.run(mainModuleModel::commit);
+        consumer.accept(mainModuleModel.getModule());
 
-		consumer.accept(mainModuleModel.getModule());
+        StartupManager.getInstance(project).registerPostStartupActivity((DumbAwareRunnable) () -> {
+            MSBuildDaemonService.getInstance(project).forceUpdate();
 
-		StartupManager.getInstance(project).registerPostStartupActivity((DumbAwareRunnable) () -> {
-			MSBuildDaemonService.getInstance(project).forceUpdate();
-
-			// TODO [VISTALL] create run configurations after reimport
-		});
-	}
+            // TODO [VISTALL] create run configurations after reimport
+        });
+    }
 }
--- msbuild-impl/src/main/java/consulo/msbuild/impl/importProvider/SolutionModuleImportProvider.java
+++ msbuild-impl/src/main/java/consulo/msbuild/impl/importProvider/SolutionModuleImportProvider.java
@@ -4,12 +4,13 @@ import consulo.annotation.access.RequiredReadAction;
 import consulo.annotation.component.ExtensionImpl;
 import consulo.application.WriteAction;
 import consulo.ide.moduleImport.ModuleImportProvider;
+import consulo.localize.LocalizeValue;
 import consulo.module.ModifiableModuleModel;
 import consulo.module.Module;
 import consulo.module.content.ModuleRootManager;
 import consulo.module.content.layer.ModifiableRootModel;
-import consulo.msbuild.MSBuildIcons;
 import consulo.msbuild.daemon.impl.MSBuildDaemonService;
+import consulo.msbuild.icon.MSBuildIconGroup;
 import consulo.msbuild.impl.VisualStudioSolutionFileType;
 import consulo.msbuild.importProvider.SolutionModuleImportContext;
 import consulo.msbuild.module.extension.MSBuildSolutionMutableModuleExtension;
@@ -22,10 +23,9 @@ import consulo.virtualFileSystem.LocalFileSystem;
 import consulo.virtualFileSystem.VirtualFile;
 import consulo.virtualFileSystem.fileType.FileTypeRegistry;
 import consulo.virtualFileSystem.util.VirtualFileUtil;
-import org.intellij.lang.annotations.Language;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.io.File;
 import java.util.function.Consumer;
 
@@ -34,147 +34,119 @@ import java.util.function.Consumer;
  * @since 01-Feb-17
  */
 @ExtensionImpl
-public class SolutionModuleImportProvider implements ModuleImportProvider<SolutionModuleImportContext>
-{
-	@Nonnull
-	@Override
-	public SolutionModuleImportContext createContext(@Nullable Project project)
-	{
-		return new SolutionModuleImportContext(project);
-	}
+public class SolutionModuleImportProvider implements ModuleImportProvider<SolutionModuleImportContext> {
+    @Nonnull
+    @Override
+    public SolutionModuleImportContext createContext(@Nullable Project project) {
+        return new SolutionModuleImportContext(project);
+    }
 
-	@Nonnull
-	@Override
-	@Language("HTML")
-	public String getFileSample()
-	{
-		return "<b>Visual Studio</b> solution file (*.sln)";
-	}
+    @Nonnull
+    @Override
+    public LocalizeValue getName() {
+        return LocalizeValue.localizeTODO("Visual Studio");
+    }
 
-	@Nonnull
-	@Override
-	public String getName()
-	{
-		return "Visual Studio";
-	}
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return MSBuildIconGroup.visualstudio();
+    }
 
-	@Override
-	public Image getIcon()
-	{
-		return MSBuildIcons.VisualStudio;
-	}
+    @Override
+    public boolean canImport(@Nonnull File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory()) {
+            // special case. We don't provide importing Unity project via solution file
+            if (new File(fileOrDirectory, "ProjectSettings/ProjectSettings.asset").exists()) {
+                return false;
+            }
 
-	@Override
-	public boolean canImport(@Nonnull File fileOrDirectory)
-	{
-		if(fileOrDirectory.isDirectory())
-		{
-			// special case. We don't provide importing Unity project via solution file
-			if(new File(fileOrDirectory, "ProjectSettings/ProjectSettings.asset").exists())
-			{
-				return false;
-			}
+            return findSingleSolutionFile(fileOrDirectory) != null;
+        }
+        else {
+            return FileTypeRegistry.getInstance().getFileTypeByFileName(fileOrDirectory.getName()) == VisualStudioSolutionFileType.INSTANCE;
+        }
+    }
 
-			return findSingleSolutionFile(fileOrDirectory) != null;
-		}
-		else
-		{
-			return FileTypeRegistry.getInstance().getFileTypeByFileName(fileOrDirectory.getName()) == VisualStudioSolutionFileType.INSTANCE;
-		}
-	}
+    @Override
+    public String getPathToBeImported(@Nonnull VirtualFile file) {
+        if (file.isDirectory()) {
+            File solutionFile = findSingleSolutionFile(VirtualFileUtil.virtualToIoFile(file));
+            if (solutionFile != null) {
+                return solutionFile.getPath();
+            }
+        }
+        return file.getPath();
+    }
 
-	@Override
-	public String getPathToBeImported(@Nonnull VirtualFile file)
-	{
-		if(file.isDirectory())
-		{
-			File solutionFile = findSingleSolutionFile(VirtualFileUtil.virtualToIoFile(file));
-			if(solutionFile != null)
-			{
-				return solutionFile.getPath();
-			}
-		}
-		return file.getPath();
-	}
+    public static File findSingleSolutionFile(File directory) {
+        File firstSolution = null;
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        if (FileTypeRegistry.getInstance().getFileTypeByFileName(file.getName()) == VisualStudioSolutionFileType.INSTANCE) {
+                            // already found - return null
+                            if (firstSolution != null) {
+                                return null;
+                            }
 
-	public static File findSingleSolutionFile(File directory)
-	{
-		File firstSolution = null;
-		if(directory.isDirectory())
-		{
-			File[] files = directory.listFiles();
-			if(files != null)
-			{
-				for(File file : files)
-				{
-					if(file.isFile())
-					{
-						if(FileTypeRegistry.getInstance().getFileTypeByFileName(file.getName()) == VisualStudioSolutionFileType.INSTANCE)
-						{
-							// already found - return null
-							if(firstSolution != null)
-							{
-								return null;
-							}
+                            firstSolution = file;
+                        }
+                    }
+                }
+            }
+        }
+        return firstSolution;
+    }
 
-							firstSolution = file;
-						}
-					}
-				}
-			}
-		}
-		return firstSolution;
-	}
+    @Override
+    public void buildSteps(@Nonnull Consumer<WizardStep<SolutionModuleImportContext>> consumer, @Nonnull SolutionModuleImportContext context) {
+        consumer.accept(new MSBuildProjectOrModuleNameStep<>(context));
+    }
 
-	@Override
-	public void buildSteps(@Nonnull Consumer<WizardStep<SolutionModuleImportContext>> consumer, @Nonnull SolutionModuleImportContext context)
-	{
-		consumer.accept(new MSBuildProjectOrModuleNameStep<>(context));
-	}
+    @RequiredReadAction
+    @Override
+    public void process(@Nonnull SolutionModuleImportContext context,
+                        @Nonnull Project project,
+                        @Nonnull ModifiableModuleModel modifiableModuleModel,
+                        @Nonnull Consumer<Module> consumer) {
+        String fileToImport = context.getFileToImport();
 
-	@RequiredReadAction
-	@Override
-	public void process(@Nonnull SolutionModuleImportContext context,
-						@Nonnull Project project,
-						@Nonnull ModifiableModuleModel modifiableModuleModel,
-						@Nonnull Consumer<Module> consumer)
-	{
-		String fileToImport = context.getFileToImport();
+        VirtualFile solutionFile = LocalFileSystem.getInstance().findFileByPath(fileToImport);
+        assert solutionFile != null;
 
-		VirtualFile solutionFile = LocalFileSystem.getInstance().findFileByPath(fileToImport);
-		assert solutionFile != null;
+        VirtualFile parent = solutionFile.getParent();
 
-		VirtualFile parent = solutionFile.getParent();
+        final ModifiableRootModel mainModuleModel = createModuleWithSingleContent(parent.getName(), parent, modifiableModuleModel);
 
-		final ModifiableRootModel mainModuleModel = createModuleWithSingleContent(parent.getName(), parent, modifiableModuleModel);
+        MSBuildSolutionMutableModuleExtension<?> solExtension = mainModuleModel.getExtensionWithoutCheck(context.getProvider().getSolutionModuleExtensionId());
+        assert solExtension != null;
+        solExtension.setEnabled(true);
+        solExtension.setSolutionFileUrl(solutionFile.getUrl());
+        solExtension.setSdkName(context.getMSBuildBundleName());
+        solExtension.setProcessProviderId(context.getProvider().getId());
 
-		MSBuildSolutionMutableModuleExtension<?> solExtension = mainModuleModel.getExtensionWithoutCheck(context.getProvider().getSolutionModuleExtensionId());
-		assert solExtension != null;
-		solExtension.setEnabled(true);
-		solExtension.setSolutionFileUrl(solutionFile.getUrl());
-		solExtension.setSdkName(context.getMSBuildBundleName());
-		solExtension.setProcessProviderId(context.getProvider().getId());
+        WriteAction.run(mainModuleModel::commit);
 
-		WriteAction.run(mainModuleModel::commit);
+        consumer.accept(mainModuleModel.getModule());
 
-		consumer.accept(mainModuleModel.getModule());
+        StartupManager.getInstance(project).registerPostStartupActivity((StartupActivity.DumbAware) (ui, porject) -> {
+            MSBuildDaemonService.getInstance(project).forceUpdate();
 
-		StartupManager.getInstance(project).registerPostStartupActivity((StartupActivity.DumbAware) (ui, porject) -> {
-			MSBuildDaemonService.getInstance(project).forceUpdate();
+            // TODO [VISTALL] create run configurations after reimport
+        });
+    }
 
-			// TODO [VISTALL] create run configurations after reimport
-		});
-	}
+    @RequiredReadAction
+    public static ModifiableRootModel createModuleWithSingleContent(String dirName, VirtualFile dir, ModifiableModuleModel modifiableModuleModel) {
+        Module module = modifiableModuleModel.newModule(dirName + " (Root)", dir.getPath());
 
-	@RequiredReadAction
-	public static ModifiableRootModel createModuleWithSingleContent(String dirName, VirtualFile dir, ModifiableModuleModel modifiableModuleModel)
-	{
-		Module module = modifiableModuleModel.newModule(dirName + " (Root)", dir.getPath());
+        ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
+        ModifiableRootModel modifiableModel = moduleRootManager.getModifiableModel();
+        modifiableModel.addContentEntry(dir);
 
-		ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
-		ModifiableRootModel modifiableModel = moduleRootManager.getModifiableModel();
-		modifiableModel.addContentEntry(dir);
-
-		return modifiableModel;
-	}
+        return modifiableModel;
+    }
 }
