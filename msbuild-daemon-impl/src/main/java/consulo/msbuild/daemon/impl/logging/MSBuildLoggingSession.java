97dffca5f0643a165a78807319b0fddf5e255d87
Nikolay Yurchenko <nikolay@yurchenko.su>
2026-06-24 14:48:55 +0300

    Fixing compilation after changes in platform. Some refactoring (#35)
    * Fixing compilation after changes in platform. Some refactoring.
    
    * Fixes.

--- msbuild-daemon-impl/src/main/java/consulo/msbuild/daemon/impl/logging/MSBuildLoggingSession.java
+++ msbuild-daemon-impl/src/main/java/consulo/msbuild/daemon/impl/logging/MSBuildLoggingSession.java
@@ -13,129 +13,109 @@ import consulo.msbuild.daemon.impl.message.model.LogMessage;
 import consulo.msbuild.localize.MSBuildLocalize;
 import consulo.project.Project;
 import consulo.util.lang.StringUtil;
-import org.jetbrains.annotations.Nls;
-
 import jakarta.annotation.Nonnull;
 
 /**
  * @author VISTALL
- * @since 20/01/2021
+ * @since 2021-01-20
  */
-public class MSBuildLoggingSession implements Disposable
-{
-	private static final String ANSI_RESET = "\u001B[0m";
-	private static final String ANSI_RED = "\u001B[31m";
-	private static final String ANSI_YELLOW = "\u001B[33m";
-	private static final String ANSI_BOLD = "\u001b[1m";
+public class MSBuildLoggingSession implements Disposable {
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BOLD = "\u001b[1m";
 
-	private static class ConsolePrinter
-	{
-		@Nonnull
-		private final BuildProgress<BuildProgressDescriptor> progress;
-		private volatile boolean isNewLinePosition = true;
+    private static class ConsolePrinter {
+        @Nonnull
+        private final BuildProgress<BuildProgressDescriptor> progress;
+        private volatile boolean isNewLinePosition = true;
 
-		private ConsolePrinter(@Nonnull BuildProgress<BuildProgressDescriptor> progress)
-		{
-			this.progress = progress;
-		}
+        private ConsolePrinter(@Nonnull BuildProgress<BuildProgressDescriptor> progress) {
+            this.progress = progress;
+        }
 
-		private void print(@Nonnull String message, @Nonnull MessageEvent.Kind kind)
-		{
-			String text = wrapWithAnsiColor(kind, message);
-			if(!isNewLinePosition && !StringUtil.startsWithChar(message, '\r'))
-			{
-				text = '\n' + text;
-			}
-			isNewLinePosition = StringUtil.endsWithLineBreak(message);
-			progress.output(text, kind != MessageEvent.Kind.ERROR);
-		}
+        private void print(@Nonnull String message, @Nonnull MessageEvent.Kind kind) {
+            String text = wrapWithAnsiColor(kind, message);
+            if (!isNewLinePosition && !StringUtil.startsWithChar(message, '\r')) {
+                text = '\n' + text;
+            }
+            isNewLinePosition = StringUtil.endsWithLineBreak(message);
+            progress.output(text, kind != MessageEvent.Kind.ERROR);
+        }
 
-		@Nls
-		private static String wrapWithAnsiColor(MessageEvent.Kind kind, @Nls String message)
-		{
-			if(kind == MessageEvent.Kind.SIMPLE)
-			{
-				return message;
-			}
-			String color;
-			if(kind == MessageEvent.Kind.ERROR)
-			{
-				color = ANSI_RED;
-			}
-			else if(kind == MessageEvent.Kind.WARNING)
-			{
-				color = ANSI_YELLOW;
-			}
-			else
-			{
-				color = ANSI_BOLD;
-			}
-			final String ansiReset = ANSI_RESET;
-			return color + message + ansiReset;
-		}
-	}
+        private static String wrapWithAnsiColor(MessageEvent.Kind kind, String message) {
+            if (kind == MessageEvent.Kind.SIMPLE) {
+                return message;
+            }
+            String color;
+            if (kind == MessageEvent.Kind.ERROR) {
+                color = ANSI_RED;
+            }
+            else if (kind == MessageEvent.Kind.WARNING) {
+                color = ANSI_YELLOW;
+            }
+            else {
+                color = ANSI_BOLD;
+            }
+            return color + message + ANSI_RESET;
+        }
+    }
 
-	private final int myId;
-	private final Project myProject;
-	private final LocalizeValue myLoggingGroup;
+    private final int myId;
+    private final Project myProject;
+    private final LocalizeValue myLoggingGroup;
 
-	private final BuildProgress<BuildProgressDescriptor> myBuildProgress;
+    private final BuildProgress<BuildProgressDescriptor> myBuildProgress;
 
-	private final ConsolePrinter myConsolePrinter;
+    private final ConsolePrinter myConsolePrinter;
 
-	public MSBuildLoggingSession(int id, Project project, @Nonnull LocalizeValue loggingGroup)
-	{
-		myId = id;
-		myProject = project;
-		myLoggingGroup = loggingGroup;
-		myBuildProgress = BuildViewManager.getInstance(project).createBuildProgress();
-		myConsolePrinter = new ConsolePrinter(myBuildProgress);
-	}
+    public MSBuildLoggingSession(int id, Project project, @Nonnull LocalizeValue loggingGroup) {
+        myId = id;
+        myProject = project;
+        myLoggingGroup = loggingGroup;
+        myBuildProgress = BuildViewManager.getInstance(project).createBuildProgress();
+        myConsolePrinter = new ConsolePrinter(myBuildProgress);
+    }
 
-	public int getId()
-	{
-		return myId;
-	}
+    public int getId() {
+        return myId;
+    }
 
-	public void acceptMessage(LogMessage logMessage)
-	{
-		acceptMessage(logMessage.LogText, MessageEvent.Kind.INFO);
-	}
+    public void acceptMessage(@Nonnull LogMessage logMessage) {
+        acceptMessage(logMessage.LogText, MessageEvent.Kind.INFO);
+    }
 
-	public void acceptMessage(String text, MessageEvent.Kind kind)
-	{
-		myConsolePrinter.print(text, kind);
-	}
+    public void acceptMessage(@Nonnull String text, @Nonnull MessageEvent.Kind kind) {
+        myConsolePrinter.print(text, kind);
+    }
 
-	public void start(ProgressIndicator indicator)
-	{
-		DefaultBuildDescriptor buildDescriptor = new DefaultBuildDescriptor(myId, MSBuildLocalize.msbuildName(), StringUtil.notNullize(myProject.getBasePath()), System.currentTimeMillis());
-		myBuildProgress.start(new BuildProgressDescriptor()
-		{
-			@Nonnull
-			@Override
-			public LocalizeValue getTitle()
-			{
-				return myLoggingGroup;
-			}
+    public void start(ProgressIndicator indicator) {
+        DefaultBuildDescriptor buildDescriptor = new DefaultBuildDescriptor(
+            myId,
+            MSBuildLocalize.msbuildName(),
+            StringUtil.notNullize(myProject.getBasePath()),
+            System.currentTimeMillis()
+        );
+        myBuildProgress.start(new BuildProgressDescriptor() {
+            @Nonnull
+            @Override
+            public LocalizeValue getTitle() {
+                return myLoggingGroup;
+            }
 
-			@Override
-			@Nonnull
-			public BuildDescriptor getBuildDescriptor()
-			{
-				return buildDescriptor;
-			}
-		});
-	}
+            @Override
+            @Nonnull
+            public BuildDescriptor getBuildDescriptor() {
+                return buildDescriptor;
+            }
+        });
+    }
 
-	public void stop()
-	{
-		myBuildProgress.finish(System.currentTimeMillis());
-	}
+    public void stop() {
+        myBuildProgress.finish(System.currentTimeMillis());
+    }
 
-	@Override
-	public void dispose()
-	{
-
-	}
+    @Override
+    public void dispose() {
+    }
 }
